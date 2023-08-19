3ddb08f2ef778c0fb043aeaefefc24dc14ebb27a
jeffrey suarez <79020208+JeffreySuarez@users.noreply.github.com>
2023-08-19 10:10:08 -0500

    express

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require("express");
-const server = express();
-const port = 3000;
+const express = require("express"); //importamos expresss desde el node_modules
+const server = express(); // lo guardamos en una variable
+const port = 3000; // en el puerto levantamos la instancia del server
 
+//ejemplo al endpoint "/"
 server.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+server.get("/ejemplo", (req, res) => {
+  res.send("Estoy en la ruta de ejemplo");
 });
 
 server.listen(port, () => {
